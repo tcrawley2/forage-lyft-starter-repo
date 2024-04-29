d8ac09b05d1133afda68cb5c8c2f3d57bfca2c1a
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-04-29 15:17:27 -0700

    added tires functionality to cars and car factory

--- car.py
+++ car.py
@@ -2,11 +2,11 @@ from serviceable import Serviceable
 
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
-        super().__init__(engine, battery)
+    def __init__(self, engine, battery, tires):
+        super().__init__(engine, battery, tires)
 
     def needs_service(self):
-        if self.engine.needs_service() or self.battery.needs_service():
+        if self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service():
             return True
         else:
             return False
--- serviceable.py
+++ serviceable.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
 class Serviceable(ABC):
-  def __init__(self, engine, battery):
+  def __init__(self, engine, battery, tires):
     self.engine = engine
     self.battery = battery
+    self.tires = tires
   
   @abstractmethod
   def needs_service(self):
